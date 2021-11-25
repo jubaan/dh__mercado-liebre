8a2df2876251dc04a8e714cb1ecd754159021fe9
Julio Añoveros <julio.ab@outlook.com>
2021-11-25 06:34:53 -0600

    Add forms and js validator

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/register.html'))
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
 app.listen(port, () =>
   console.log(
     `Server running in port ${port}, please visit https:localhost:${port}`
